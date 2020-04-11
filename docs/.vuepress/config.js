f857dd37442ca1b87af3acad122901898e6b01ff
yanyh9747 <yanyh9747@189.cn>
2020-04-11 14:55:50 +0800

    测试增加markdown文件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,9 @@ module.exports = {
   title: 'VuePress 在21云盒子',
   description: '一简单的VuePress项目部署在21云盒子',
   themeConfig: {
-    nav: [{ text: 'Home', link: '/' }]
+    nav: [
+		{ text: '主页', link: '/' },
+		{ text: '目录', link: '/catalog/' },
+		]
   }
 }
